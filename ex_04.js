aad1487da4b364aa86218a92b74f301196a492ea
EM02-AL <eddy.mazi@epitech.digital>
2024-09-03 11:24:50 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,1 +1,14 @@
-
+var string1 = "undefined"
+var boolean = "boolean"
+var string2 = "number"
+var bigint = "bigint"
+var symbol = "symbol"
+var object = "object"
+var string3 = "function"
+displayThisText(string1)
+displayThisText(boolean)
+displayThisText(string2)
+displayThisText(bigint)
+displayThisText(symbol)
+displayThisText(object)
+displayThisText(string3)
