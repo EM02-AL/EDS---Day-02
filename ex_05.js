56d76e92b2f3f277394574b8f0fc1d85048557f3
EM02-AL <eddy.mazi@epitech.digital>
2024-09-03 11:38:45 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,1 +1,7 @@
-
+var firstname = "John"
+var lastname = "Doe"
+var cardCode = "1111 2222 3333 4444"
+var expiryDateMonth = 21
+var expiryDateYear = 2042
+var computedBankInfo = "John, Doe, 111 2222 3333 4444, 21/2042"
+displayInCard(computedBankInfo)
