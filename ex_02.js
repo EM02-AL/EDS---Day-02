fa632b5394a2e3bde1694e15ee7e4050488e4e8b
EM02-AL <eddy.mazi@epitech.digital>
2024-09-03 11:05:57 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,10 @@
-
+var integer = 42
+var float = 42.42
+var string = "forty two"
+var bool = true
+var this_is_null = null
+displayThisText(bool)
+displayThisText(string)
+displayThisText(integer)
+displayThisText(float)
+displayThisText(this_is_null)
